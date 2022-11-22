9f7751ec3dc276edd8e5e465be17f7a1d8cd25be
ianK-code <kian99564@gmail.com>
2022-11-22 11:56:31 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+// let feet = 300
+function scuberGreetingForFeet(feet){
   // Write your code here!
-}
 
-function ternaryCheckCity(){
-  // Write your code here!
+  if (feet < 400){
+    return 'This one is on me!' //This works
+  }else if(feet > 2000 && feet <= 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if(feet > 2500){
+    return 'No can do.'
+  }
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === 'NYC'){
+    return 'Ok, sounds good.'
+  }
+  else if(city !== 'NYC'){
+    return 'No go.'
+  }
+}
+function switchOnCharmFromTip(tip){
+  if(tip === 'generous'){
+    return 'Thank you so much.'
+  }
+  else if(tip === 'not as generous'){
+    return 'Thank you.'
+  }
+  else{
+    return 'Bye.'
+  }
 }
